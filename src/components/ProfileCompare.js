254de80eadce6c0662de86352ceb6927bcdf01e9
Josh <ahimaz.joshua@gmail.com>
2020-11-03 14:03:23 +0800

    optimised for mobile

--- src/components/ProfileCompare.js
+++ src/components/ProfileCompare.js
@@ -41,7 +41,6 @@ function ProfileCompare(){
             fetch(`https://api.github.com/users/${secondUserInput}`).then(user2 => user2.json())
             // fetch(`https://api.github.com/users/${secondUserInput}/repos`)
           ]).then((data) => {
-                setUserData(true);
                 if(data[0].message){
                     setErrorUser1(data[0].message);
                 }else{
@@ -53,15 +52,16 @@ function ProfileCompare(){
                 }else{
                     setErrorUser2(null);
                     setData(data[1], "user2");
-                }              
+                }
+                setUserData(true);
           });
       }
     
       const setData = ({ name, login, followers, following, public_repos, avatar_url }, user) => {
         if(user == "user1"){
-          setFirstUser({name: name, userName: login, followers: followers, following: following, repos: public_repos, avatar: avatar_url});
+          setFirstUser({name: name, userName: login, followers: followers, following: following, repoCount: public_repos, avatar: avatar_url});
         }else if(user == "user2"){
-          setSecondUser({name: name, userName: login, followers: followers, following: following, repos: public_repos, avatar: avatar_url});
+          setSecondUser({name: name, userName: login, followers: followers, following: following, repoCount: public_repos, avatar: avatar_url});
         }
       }
     
@@ -70,12 +70,14 @@ function ProfileCompare(){
             <div className="Search-bar">
             <Form onSubmit={handleSearchSubmit}>
               <Form.Group as={Row}>
-                <Col>
+                <Col md={5} sm={12}>
                     <Form.Label className="label">User Profile 1</Form.Label>
                     <Form.Control type="text" placeholder="Github Profile" id="user1" className="input-fields" onChange={handleUserProfileInput}/>
                 </Col>
+                <Col md={2} sm={12}>
                 <p className="label">vs</p>
-                <Col>
+                </Col>
+                <Col md={5} sm={12}>
                     <Form.Label className="label">User Profile 2</Form.Label>
                     <Form.Control type="text" placeholder="Github Profile" id="user2" className="input-fields" onChange={handleUserProfileInput}/>
                 </Col>
@@ -89,79 +91,103 @@ function ProfileCompare(){
             ) : (
             <div>
                 <Row>
-                    <Col className="Card">
+                    <Col className="Card" sm={12} md={6}>
                     <div className="Github-card">
                         { user1Error ? (<div className="error-text">{user1Error}</div>) : 
                         (
                             <div>
                                 <Row>
-                            <Col sm="5">
+                            <Col sm={12} md={5}>
                                 <img src={user1.avatar} className="profile-image" />
                             </Col>
-                            <Col sm="87">
+                            <Col sm={12} md={7}>
                             <p className="profile-header"><span className="profile-name">{user1.name}</span><br/>
                             <a className="profile-link" href={"https://Github.com/" + user1.userName}>Github.com/{user1.userName} <FontAwesomeIcon icon={faExternalLinkAlt} /></a></p>
                             </Col>
                         </Row>
                         <Row className="profile-follow">
-                            <Col sm="6">
-                                <div  className="follow-box">
+                            <Col md="6">
+                                <div  className="profile-box">
                                     <p>
                                         Followers: <br/>
-                                        {user1.followers > user2.followers && <span className="follow-number-greater">{user1.followers}</span>}
-                                        {user2.followers > user1.followers && <span className="follow-number-lower">{user1.followers}</span>}
-                                        {user2.followers === user1.followers && <span className="follow-number-same">{user1.followers}</span>}
+                                        {user1.followers > user2.followers && <span className="number-greater">{user1.followers}</span>}
+                                        {user2.followers > user1.followers && <span className="number-lower">{user1.followers}</span>}
+                                        {user2.followers === user1.followers && <span className="number-same">{user1.followers}</span>}
                                     </p>
                                 </div>
                             </Col>
-                            <Col sm="6">
-                                <div  className="follow-box">
+                            <Col md="6">
+                                <div  className="profile-box">
                                     <p>
                                         Following: <br/>
-                                        {user1.following > user2.following && <span className="follow-number-greater">{user1.following}</span>}
-                                        {user2.following > user1.following && <span className="follow-number-lower">{user1.following}</span>}
-                                        {user2.following === user1.following && <span className="follow-number-same">{user1.following}</span>}
+                                        {user1.following > user2.following && <span className="number-greater">{user1.following}</span>}
+                                        {user2.following > user1.following && <span className="number-lower">{user1.following}</span>}
+                                        {user2.following === user1.following && <span className="number-same">{user1.following}</span>}
                                     </p>
                                 </div>
                             </Col>
                         </Row>
-                            </div>
+                        <Row className="profile-follow">
+                            <Col md={12}>
+                                <div className="profile-box">
+                                    <p>
+                                        Repositories: <br/>
+                                        {user1.repoCount > user2.repoCount && <span className="number-greater">{user1.repoCount}</span>}
+                                        {user2.repoCount > user1.repoCount && <span className="number-lower">{user1.repoCount}</span>}
+                                        {user2.repoCount === user1.repoCount && <span className="number-same">{user1.repoCount}</span>}
+                                    </p>
+                                </div>
+                            </Col>
+                        </Row>
+                        </div>
                         )
                         } 
                     </div>
                     </Col>
-                    <Col className="Card">
+                    <Col className="Card" sm={12} md={6}>
                     <div className="Github-card">
                         { user2Error ? (<div className="error-text">{user2Error}</div>) : 
                         (
                         <div>
                         <Row>
-                            <Col sm="5">
+                            <Col sm={12} md={"5"} >
                                 <img src={user2.avatar} className="profile-image" />
                             </Col>
-                            <Col sm="87">
+                            <Col sm={12} md={"7"}>
                             <p className="profile-header"><span className="profile-name">{user2.name}</span><br/>
                             <a className="profile-link" href={"https://Github.com/" + user2.userName}>Github.com/{user2.userName} <FontAwesomeIcon icon={faExternalLinkAlt} /></a></p>
                             </Col>
                         </Row>
                         <Row className="profile-follow">
                         <Col sm="6">
-                                <div  className="follow-box">
+                                <div  className="profile-box">
                                     <p>
                                         Followers: <br/>
-                                        {user2.followers > user1.followers && <span className="follow-number-greater">{user2.followers}</span>}
-                                        {user1.followers > user2.followers && <span className="follow-number-lower">{user2.followers}</span>}
-                                        {user1.followers === user2.followers && <span className="follow-number-same">{user2.followers}</span>}
+                                        {user2.followers > user1.followers && <span className="number-greater">{user2.followers}</span>}
+                                        {user1.followers > user2.followers && <span className="number-lower">{user2.followers}</span>}
+                                        {user1.followers === user2.followers && <span className="number-same">{user2.followers}</span>}
                                     </p>
                                 </div>
                             </Col>
                             <Col sm="6">
-                                <div  className="follow-box">
+                                <div  className="profile-box">
                                     <p>
                                         Following: <br/>
-                                        {user2.following > user1.following && <span className="follow-number-greater">{user2.following}</span>}
-                                        {user1.following > user2.following && <span className="follow-number-lower">{user2.following}</span>}
-                                        {user1.following === user2.following && <span className="follow-number-same">{user2.following}</span>}
+                                        {user2.following > user1.following && <span className="number-greater">{user2.following}</span>}
+                                        {user1.following > user2.following && <span className="number-lower">{user2.following}</span>}
+                                        {user1.following === user2.following && <span className="number-same">{user2.following}</span>}
+                                    </p>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row className="profile-follow">
+                            <Col md={12}>
+                                <div className="profile-box">
+                                    <p>
+                                        Repositories: <br/>
+                                        {user2.repoCount > user1.repoCount && <span className="number-greater">{user2.repoCount}</span>}
+                                        {user1.repoCount > user2.repoCount && <span className="number-lower">{user2.repoCount}</span>}
+                                        {user1.repoCount === user2.repoCount && <span className="number-same">{user2.repoCount}</span>}
                                     </p>
                                 </div>
                             </Col>
